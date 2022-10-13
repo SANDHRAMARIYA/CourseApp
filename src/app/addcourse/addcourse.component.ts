5b716031655a7a467531b8e5f8b62695112a2e71
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-13 22:56:34 +0530

    serce

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcourse',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
+
+  courseTitle=""
+  courseDuration=""
+  courseDescription=""
+  courseDate=""
+  courseVenue=""
+
+  readValues=()=>{
+    let data = {
+      "courseTitle":this.courseTitle,
+    "courseDuration":this.courseDuration,
+    "courseDescription":this.courseDescription,
+    "courseDate":this.courseDate,
+    "courseVenue":this.courseVenue
+    }
+    console.log(data)
+    this.myapi.addCourse(data).subscribe(
+      (response)=>{
+        console.log(response)
+        alert("succesfully added")
+      }
+    )
+  }
+
+
+
 
   ngOnInit(): void {
   }
